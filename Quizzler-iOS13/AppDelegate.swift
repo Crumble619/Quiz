e2aeef6329c77bad8d2410d70324635bd701b09c
Colin Greene <colinpatrickgreene@gmail.com>
2021-09-28 16:40:31 -0700

    Google Interstitial Ads Implemented.

--- Quizzler-iOS13/AppDelegate.swift
+++ Quizzler-iOS13/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import GoogleMobileAds
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        GADMobileAds.sharedInstance().start(completionHandler: nil)
+        GADMobileAds.sharedInstance().requestConfiguration.testDeviceIdentifiers = [ "b72244712d49e74907680d7ff607a63c" ]
+        
         return true
     }
 
--- Quizzler-iOS13/Controller/ResultsViewController.swift
+++ Quizzler-iOS13/Controller/ResultsViewController.swift
@@ -6,9 +6,12 @@
 //  Copyright © 2021 The App Brewery. All rights reserved.
 //
 
+import GoogleMobileAds
 import UIKit
 
-class ResultsViewController: UIViewController {
+class ResultsViewController: UIViewController, GADFullScreenContentDelegate {
+    
+    private var interstitial: GADInterstitialAd?
     
     var quizBrain = QuizBrain()
     var scoreFromQuiz: Int = 0
@@ -19,10 +22,32 @@ class ResultsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let request = GADRequest()
+        GADInterstitialAd.load(withAdUnitID:"ca-app-pub-3940256099942544/4411468910", request: request, completionHandler: { [self] ad, error in
+            if let error = error {
+                print("Failed to load interstitial ad with error: \(error.localizedDescription)")
+                return
+            }
+            interstitial = ad
+            interstitial?.fullScreenContentDelegate = self
+        })
+        
         resultScoreLabel.text = "\(scoreFromQuiz) / \(quizBrain.quiz.count)"
         highScoreLabel.text = "\(quizBrain.getHighScore()) / \(quizBrain.quiz.count)"
+        
+        
+        Timer.scheduledTimer(timeInterval: 3.0, target: self, selector: #selector(adTimer), userInfo: nil, repeats: false)
+        
 
         // Do any additional setup after loading the view.
+    }
+    
+    @objc func adTimer() {
+        if interstitial != nil {
+            interstitial!.present(fromRootViewController: self)
+        } else {
+            print("Ad wasn't ready")
+        }
     }
 
     @IBAction func tryAgainPressed(_ sender: UIButton) {
@@ -30,6 +55,21 @@ class ResultsViewController: UIViewController {
         quizBrain.score = 0
         performSegue(withIdentifier: "backToQuiz", sender: self)
     }
+    
+    func ad(_ ad: GADFullScreenPresentingAd, didFailToPresentFullScreenContentWithError error: Error) {
+        print("Ad did fail to present full screen content.")
+      }
+
+      /// Tells the delegate that the ad presented full screen content.
+      func adDidPresentFullScreenContent(_ ad: GADFullScreenPresentingAd) {
+        print("Ad did present full screen content.")
+      }
+
+      /// Tells the delegate that the ad dismissed full screen content.
+      func adDidDismissFullScreenContent(_ ad: GADFullScreenPresentingAd) {
+        print("Ad did dismiss full screen content.")
+      }
+    
     /*
     // MARK: - Navigation
 
